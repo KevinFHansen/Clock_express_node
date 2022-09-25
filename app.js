6987cc2bc5358d55f5049b55b9f5dbd0473db527
KevinFHansen <kevi4777@stud.kea.dk>
2022-09-25 20:01:40 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express();
+
+app.use(express.static("public"));
+
+import path from "path";
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("public/frontpage.html"));
+});
+
+app.get("/clock", (req, res) => {
+    res.send({ data: ["23:58"] });
+});
+
+
+app.listen(8080, () => {
+    console.log("Listening on port", 8080)
+});
---
+++ public/frontpage.js
@@ -0,0 +1,1 @@
+//import { getTimeStamp } from "./time.js";

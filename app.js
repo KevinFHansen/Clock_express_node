4954816c40f68bd1c25a28cd0ac0bd209b570019
KevinFHansen <kevi4777@stud.kea.dk>
2022-09-25 20:55:50 +0200

    name fixes + oprydning

--- app.js
+++ app.js
@@ -9,11 +9,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.resolve("public/frontpage.html"));
 });
 
-app.get("/clock", (req, res) => {
-    res.send({ data: ["23:58"] });
-});
-
-
 app.listen(process.env.PORT || 8080, () => {
     console.log("Listening on port", 8080)
 });
--- public/frontpage.js
+++ public/frontpage.js
@@ -1,1 +1,30 @@
-//import { getTimeStamp } from "./time.js";
+let start;
+let end;
+let results = [];
+let text = document.getElementById("btn-game").addEventListener("click", toggle);
+
+function toggle(){
+    let text = document.getElementById("btn-game").innerHTML;
+    
+    if (text === "Start"){
+        document.getElementById("btn-game").innerHTML = "Stop";
+        document.getElementById("btn-game").style.backgroundColor = "red";
+        start = Date.now();
+        
+    }else{
+        end = Date.now();
+        document.getElementById("btn-game").innerHTML = "Start";
+        document.getElementById("btn-game").style.backgroundColor = "#08f100";
+        let finish = (end - start) / 1000;
+            if (Math.round(finish) === 60){
+                document.getElementById("status").innerHTML = "NAILED IT!!!"
+            }else{
+                document.getElementById("status").innerHTML = "Try again!"
+            }
+        console.log(finish.toFixed(2));
+        document.getElementById("result").innerHTML = "Your result: " + finish.toFixed(2) + " seconds";
+        results.push(finish.toFixed(2));
+        document.getElementById("results").innerHTML = results.join(" " + " | ");
+}
+
+}
